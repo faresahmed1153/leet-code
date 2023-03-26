62a42197f2b66ef7ecc48933d35c5f8b10c602a5
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-03-26 22:03:26 +0200

    Time: 250 ms (74.70%), Space: 61.7 MB (22.80%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cs
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -1,6 +1,13 @@
 public class Solution {
     public void ReverseString(char[] s) {
-        Array.Reverse(s);
+      int j = s.Length - 1;
+            for (int i = 0; i < j; i++)
+            {
+                var temp = s[i];
+                char x = s[i] = s[j];
+                char y = s[j] = temp;
+                j--;
+            }
             
     }
 }
