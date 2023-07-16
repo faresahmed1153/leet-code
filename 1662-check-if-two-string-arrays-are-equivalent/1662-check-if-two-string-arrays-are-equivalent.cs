e206d1b9896eb5efe64994696790901fe0257f54
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-07-16 13:22:55 +0300

    Delete 1662-check-if-two-string-arrays-are-equivalent directory

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cs
+++
@@ -1,7 +0,0 @@
-public class Solution {
-    public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
-        string w1 = String.Concat(word1);
-            string w2 = String.Concat(word2);
-            return w1 == w2;
-    }
-}
