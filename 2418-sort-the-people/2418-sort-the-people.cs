fd085ac0ed4140523d5e1fbad84c13c9aef8f2ff
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-07-17 13:43:19 +0300

    Time: 212 ms (36.70%), Space: 61.3 MB (8.51%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cs
+++ 2418-sort-the-people/2418-sort-the-people.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public string[] SortPeople(string[] names, int[] heights) {
-  SortedDictionary<int, string> people = new SortedDictionary<int, string>(Comparer<int>.Create((a, b) => b - a));
+Dictionary<int, string> people = new Dictionary<int, string>();
             for (int i = 0; i < names.Length; i++)
             {
                 people.Add(heights[i], names[i]);
             }
-            string[] sortedNames = people.Select(x => x.Value).ToArray();
+            string[] sorted = people.OrderByDescending(x => x.Key).Select(x => x.Value).ToArray();
 
-            return sortedNames;
+            return sorted;
 }
 }
