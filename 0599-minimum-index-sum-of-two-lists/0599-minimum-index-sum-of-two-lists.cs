2817b7df4dfa7ebbc31a66ed1e0fbab0a7acc70a
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-07-26 12:08:50 +0300

    Time: 234 ms (15.28%), Space: 59.4 MB (86.11%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cs
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cs
@@ -2,25 +2,24 @@ public class Solution {
     public string[] FindRestaurant(string[] list1, string[] list2) {
     
         int sumOfIndex = 100000000;
-            List<string> list = new List<string>();
+            LinkedList<string> list = new LinkedList<string>();
             for (int i = 0; i < list1.Length; i++)
             {
                 for (int j = 0; j < list2.Length; j++)
                 {
-                    if (list1[i]==list2[j])
+                    if (list1[i] == list2[j])
                     {
                         if (i + j < sumOfIndex)
                         {
                             sumOfIndex = i + j;
                             list.Clear();
-                            list.Add(list1[i]);
-
+                            list.AddLast(list1[i]);
 
                         }
                         else if (i + j == sumOfIndex)
                         {
                             sumOfIndex = i + j;
-                            list.Add(list1[i]);
+                            list.AddLast(list1[i]);
                         }
                     }
                 }
