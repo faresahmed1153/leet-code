c57de778fe16147243446970fc2a24215640b3ea
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-08-04 12:17:25 +0300

    Time: 659 ms (6.37%), Space: 57.1 MB (12.74%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int CountConsistentStrings(string allowed, string[] words) {
+          HashSet<char> letter = new HashSet<char>();
+            LinkedList<string> list = new LinkedList<string>(words);
+            for (int i = 0; i < allowed.Length; i++)
+
+                letter.Add(allowed[i]);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    if (!letter.Contains(words[i][j]))
+                    {
+                        list.Remove(words[i]);
+                        break;
+                    }
+
+                }
+            }
+
+            return list.Count;
+    }
+}
