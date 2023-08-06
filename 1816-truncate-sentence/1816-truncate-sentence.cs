9103afb99be62c68c794e32e7963f5796c569d89
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-08-06 13:54:32 +0300

    Delete 1816-truncate-sentence directory

--- 1816-truncate-sentence/1816-truncate-sentence.cs
+++
@@ -1,6 +0,0 @@
-public class Solution {
-    public string TruncateSentence(string s, int k) {
-
-        return string.Join(" ", (s.Split(" ", StringSplitOptions.TrimEntries).TakeWhile((word, index) => index < k)));
-    }
-}
