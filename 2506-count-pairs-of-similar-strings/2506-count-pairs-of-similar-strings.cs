5b5eda4983fc1b3bb7a3fabc1d3cc7899c38f5e7
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-08-07 16:25:04 +0300

    Time: 625 ms (10.42%), Space: 120.8 MB (6.25%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cs
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cs
@@ -1,11 +1,11 @@
 public class Solution {
     public int SimilarPairs(string[] words) {
-      int similarPairs = 0;
-            List<string> list = new List<string>();
+           int similarPairs = 0;
+            LinkedList<string> list = new LinkedList<string>();
             for (int i = 0; i < words.Length; i++)
-            {
-                list.Add(string.Concat(words[i].ToCharArray().Distinct()));
-            }
+
+                list.AddLast(string.Concat(words[i].ToCharArray().Distinct()));
+   
 
             for (int i = 0; i < list.Count; i++)
             {
@@ -13,7 +13,7 @@ public class Solution {
                 for (int j = i + 1; j < list.Count; j++)
                 
 
-                    if (string.Concat(list[i].OrderBy(x => x)) == string.Concat(list[j].OrderBy(x => x)))
+                    if (string.Concat(list.ElementAt(i).OrderBy(x => x)) == string.Concat(list.ElementAt(j).OrderBy(x => x)))
                     
                         similarPairs++;
             }
