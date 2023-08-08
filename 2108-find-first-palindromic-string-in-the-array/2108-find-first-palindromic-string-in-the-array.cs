4fd31664683824e2c9aefe761835c4640b0181b9
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-08-08 14:21:21 +0300

    Time: 98 ms (92.78%), Space: 56.6 MB (28.87%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cs
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cs
@@ -2,7 +2,12 @@ public class Solution {
     public string FirstPalindrome(string[] words) {
         for (int i = 0; i < words.Length; i++)
             
-                if (words[i] == string.Concat(words[i].Reverse()))
+                if (words[i] == string.Create(words[i].Length, words[i], (chars, state) =>
+                {
+                    state.AsSpan().CopyTo(chars);
+                    chars.Reverse();
+                }))
+                    
                     return words[i];
                    
             return string.Empty;
