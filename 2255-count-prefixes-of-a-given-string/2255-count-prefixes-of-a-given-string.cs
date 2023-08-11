799ecd846b460bb4ed600dead007afb70ccc6e2e
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-08-11 14:20:16 +0300

    Time: 87 ms (72.73%), Space: 41.3 MB (11.69%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cs
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cs
@@ -1,10 +1,5 @@
 public class Solution {
     public int CountPrefixes(string[] words, string s) {
-        int count = 0;
-            for (int i = 0; i < words.Length; i++)
-
-                if (s.StartsWith(words[i]))
-                    count++;
-            return count;
+        return words.Count(word=>s.StartsWith(word));
     }
 }
