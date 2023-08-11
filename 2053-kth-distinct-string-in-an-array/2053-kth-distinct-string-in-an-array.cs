503a9cc75cc9ffb67b304c8fc8c4647edd1fd07a
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-08-11 15:22:48 +0300

    Time: 87 ms (98.31%), Space: 43.9 MB (11.86%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public string KthDistinct(string[] arr, int k) {
+      string[] distinctArr = arr.GroupBy(w => w).Where(w => w.Count() == 1).Select(w => w.Key).ToArray();
+            
+            return distinctArr.Count() >= k ? distinctArr[k - 1] : string.Empty;
+    }
+}
