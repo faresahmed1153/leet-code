dffa4f9e75c83d5a44cb972ffc03f263b597cc5e
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-08-16 14:03:47 +0300

    Time: 154 ms (58.62%), Space: 44.1 MB (91.38%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cs
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cs
@@ -1,19 +1,29 @@
 public class Solution {
     public IList<string> StringMatching(string[] words) {
-        List<string> StringMatching = new List<string>();
+         HashSet<string> StringMatching = new HashSet<string>();
             for (int i = 0; i < words.Length; i++)
             {
                 for (int j = i + 1; j < words.Length; j++)
                 {
                     if (words[i].Contains(words[j]))
+                    
                         StringMatching.Add(words[j]);
+                        
+                    
+                        
+
                     else
                     {
                         if (words[j].Contains(words[i]))
+                        {
                             StringMatching.Add(words[i]);
+                            break;
+                        }
+                            
                     }
                 }
             }
-            return StringMatching.Distinct().ToList();
+
+            return StringMatching.ToList();
     }
 }
