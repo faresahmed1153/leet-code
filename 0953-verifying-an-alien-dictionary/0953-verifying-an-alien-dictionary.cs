19785ab30964b8c5866b6b708d2cfc09f176815f
faresahmed1153 <57008521+faresahmed1153@users.noreply.github.com>
2023-08-23 14:08:27 +0300

    Time: 93 ms (38.81%), Space: 41.2 MB (51.49%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public bool IsAlienSorted(string[] words, string order) {
+        
+       Dictionary<char, int> alien = new Dictionary<char, int>();
+            bool IsAlienSorted = true;
+            int orderOfCurrentChar;
+            int orderOfNextChar;
+            for (int i = 0; i < order.Length; i++)
+                alien.Add(order[i], i);
+
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    alien.TryGetValue(words[i][j], out orderOfCurrentChar);
+                    if (j >= words[i + 1].Length)
+                    {
+                        orderOfNextChar = -1;
+                    }
+                    else
+                    {
+                        alien.TryGetValue(words[i + 1][j], out orderOfNextChar);
+                    }
+
+                    if (orderOfCurrentChar < orderOfNextChar)
+                    {
+
+                        break;
+                    }
+                    if (orderOfNextChar < orderOfCurrentChar)
+                    {
+                        IsAlienSorted = false;
+                        break;
+                    }
+                    if (!IsAlienSorted)
+                        break;
+                }
+            }
+            return IsAlienSorted;
+    }
+}
